7c9b0746e22cdb5139165de7d46f0e1fcb50ec39
iamverg <dev01.dash@gmail.com>
2023-09-04 18:38:57 +0200

    created about and terms page component

--- app/Main.js
+++ app/Main.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeGuest from "./components/HomeGuest";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
   return (
