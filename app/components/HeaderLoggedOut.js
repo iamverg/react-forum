f7450b53eb60f4662e760759b6950b5dca093716
iamverg <dev01.dash@gmail.com>
2023-09-06 19:04:55 +0200

    handle login form submit and error message

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 export default function HeaderLoggedOut() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await Axios.post("http://localhost:8080/login", {
+        username,
+        password
+      });
+      if (response.data) {
+        console.log(response.data);
+      } else {
+        console.log("Incorrect username / password.");
+      }
+    } catch (error) {
+      console.log("There was a problem.");
+    }
+  }
   return (
-    <form className="mb-0 pt-2 pt-md-0">
+    <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
           <input
+            onChange={(e) => setUsername(e.target.value)}
             name="username"
             className="form-control form-control-sm input-dark"
             type="text"
@@ -14,6 +35,7 @@ export default function HeaderLoggedOut() {
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
           <input
+            onChange={(e) => setPassword(e.target.value)}
             name="password"
             className="form-control form-control-sm input-dark"
             type="password"
