a3931fef45c4dc499b7ffc2e3aaf2df99a47e887
iamverg <dev01.dash@gmail.com>
2023-09-06 20:34:29 +0200

    relocate loggedIn state to main component to dynamically render the login/logout page

--- app/Main.js
+++ app/Main.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeGuest from "./components/HomeGuest";
+import Home from "./components/Home";
 import About from "./components/About";
 import Terms from "./components/Terms";
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(
+    Boolean(localStorage.getItem("cAppToken"))
+  );
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
         <Routes>
-          <Route path="/" element={<HomeGuest />} />
+          <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
           <Route path="/about-us" element={<About />} />
           <Route path="/terms" element={<Terms />} />
         </Routes>
