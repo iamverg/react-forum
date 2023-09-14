01c3adfdea5c46ab9a8d9fbeb3d9ca68cec79274
iamverg <dev01.dash@gmail.com>
2023-09-14 20:30:17 +0200

    react transition group to animate search, included tooltip for profile, chat and search

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
+import { Tooltip } from "react-tooltip";
 export default function HeaderLoggedIn() {
   const appDispatch = useContext(DispatchContext);
   const appState = useContext(StateContext);
@@ -21,19 +22,33 @@ export default function HeaderLoggedIn() {
         onClick={handleSearchIcon}
         href="#"
         className="text-white mr-2 header-search-icon"
+        data-tooltip-content="Search"
+        data-tooltip-id="search"
       >
         <i className="fas fa-search"></i>
       </a>
-      <span className="mr-2 header-chat-icon text-white">
+      <Tooltip place="bottom" id="search" className="custom-tooltip" />{" "}
+      <span
+        className="mr-2 header-chat-icon text-white"
+        data-tooltip-content="Chat"
+        data-tooltip-id="chat"
+      >
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
-      <Link to={`/profile/${appState.user.username}`} className="mr-2">
+      <Tooltip place="bottom" id="chat" className="custom-tooltip" />{" "}
+      <Link
+        to={`/profile/${appState.user.username}`}
+        className="mr-2"
+        data-tooltip-content="My Profile"
+        data-tooltip-id="profile"
+      >
         <img className="small-header-avatar" src={appState.user.avatar} />
       </Link>
+      <Tooltip place="bottom" id="profile" className="custom-tooltip" />{" "}
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </Link>
+      </Link>{" "}
       <button onClick={handleLogout} className="btn btn-sm btn-secondary">
         Sign Out
       </button>
