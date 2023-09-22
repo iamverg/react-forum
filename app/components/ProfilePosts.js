0c6cdcfa8f6413bb38cd52e7095cf9b08d252451
iamverg <dev01.dash@gmail.com>
2023-09-22 21:49:32 +0200

    implement home feed, extract post to its own component

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import Post from "./Post";
+
 export default function ProfilePosts() {
   const { username } = useParams();
   const [isLoading, setIsLoading] = useState(true);
@@ -32,21 +34,7 @@ export default function ProfilePosts() {
   return (
     <div className="list-group">
       {posts.map((post) => {
-        const date = new Date(post.createdDate);
-        const dateFormatted = `${
-          date.getMonth() + 1
-        }/${date.getDate()}/${date.getFullYear()}`;
-        return (
-          <Link
-            key={post._id}
-            to={`/post/${post._id}`}
-            className="list-group-item list-group-item-action"
-          >
-            <img className="avatar-tiny" src={post.author.avatar} />
-            <strong>{post.title}</strong>
-            <span className="text-muted small"> on {dateFormatted} </span>
-          </Link>
-        );
+        return <Post post={post} key={post._id} noAuthor />;
       })}
     </div>
   );
