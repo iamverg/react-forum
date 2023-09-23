eb4df2ee88392072fa4b9bc806a228386e9770c6
iamverg <dev01.dash@gmail.com>
2023-09-23 16:15:04 +0200

    implemented dynamic chat message creation

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,17 +1,44 @@
 import React, { useContext, useEffect, useRef } from "react";
 import StateContext from "../StateContext";
 import DispatchContext from "../DispatchContext";
+import { useImmer } from "use-immer";
 
 export default function Chat() {
   const chatField = useRef(null);
   const appState = useContext(StateContext);
   const appDispatch = useContext(DispatchContext);
 
+  const [state, setState] = useImmer({
+    fieldValue: "",
+    chatMessages: []
+  });
+
   useEffect(() => {
     if (appState.isChatOpen) {
       chatField.current.focus();
     }
   }, [appState.isChatOpen]);
+
+  function handleFieldChange(e) {
+    const value = e.target.value;
+    setState((draft) => {
+      draft.fieldValue = value;
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Send message to chat server
+    setState((draft) => {
+      // Add message to state collection of messages
+      draft.chatMessages.push({
+        message: draft.fieldValue,
+        username: appState.user.username,
+        avatar: appState.user.avatar
+      });
+      draft.fieldValue = "";
+    });
+  }
 
   return (
     <div
@@ -31,35 +58,48 @@ export default function Chat() {
         </span>
       </div>
       <div id="chat" className="chat-log">
-        <div className="chat-self">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hey, how are you?</div>
-          </div>
-          <img className="chat-avatar avatar-tiny" src="" />
-        </div>
-
-        <div className="chat-other">
-          <a href="#">
-            <img className="avatar-tiny" src="" />
-          </a>
-          <div className="chat-message">
-            <div className="chat-message-inner">
+        {state.chatMessages.map((message, index) => {
+          if (message.username == appState.user.username) {
+            return (
+              <div key={index} className="chat-self">
+                <div className="chat-message">
+                  <div className="chat-message-inner">{message.message}</div>
+                </div>
+                <img className="chat-avatar avatar-tiny" src={message.avatar} />
+              </div>
+            );
+          }
+          return (
+            <div className="chat-other">
               <a href="#">
-                <strong>Test</strong>
+                <img className="avatar-tiny" src="" />
               </a>
-              Hey, I am good, how about you?
+              <div className="chat-message">
+                <div className="chat-message-inner">
+                  <a href="#">
+                    <strong>Test</strong>
+                  </a>
+                  Hey, I am good, how about you?
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-      <form id="chatForm" className="chat-form border-top">
+      <form
+        onSubmit={handleSubmit}
+        id="chatForm"
+        className="chat-form border-top"
+      >
         <input
+          onChange={handleFieldChange}
           ref={chatField}
           type="text"
           className="chat-field"
           id="chatField"
           placeholder="Type a message…"
           autoComplete="off"
+          value={state.fieldValue}
         />
       </form>
     </div>
