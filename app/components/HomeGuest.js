3bd94771a36e615d4df45150b19ced9ad657fd33
iamverg <dev01.dash@gmail.com>
2023-09-23 20:19:17 +0200

    prepare registration validation

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,24 +1,80 @@
 import React, { useState } from "react";
 import Page from "./Page";
 import Axios from "axios";
+import { useImmerReducer } from "use-immer";
+import { CSSTransition } from "react-transition-group";
+
 export default function HomeGuest() {
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const initialState = {
+    username: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      isUnique: false,
+      checkCount: 0
+    },
+    email: {
+      value: "",
+      hasErrors: false,
+      message: "",
+      isUnique: false,
+      checkCount: 0
+    },
+    password: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    submitCount: 0
+  };
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
 
-    try {
-      await Axios.post("/register", {
-        username,
-        email,
-        password
-      });
-      console.log("User was succesfully created.");
-    } catch (error) {
-      console.log(error.response.data);
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "usernameImmediately":
+        draft.username.hasErrors = false;
+        draft.username.value = action.value;
+        if (draft.username.value.length > 30) {
+          draft.username.hasErrors = true;
+          draft.username.message = "Username cannot exceed 30 characters.";
+        }
+        if (
+          draft.username.value &&
+          !/^([a-zA-Z0-9]+)$/.test(draft.username.value)
+        ) {
+          draft.username.hasErrors = true;
+          draft.username.message =
+            "Username can only contain letters and numbers.";
+        }
+        break;
+      case "usernameAfterDelay":
+        break;
+      case "userNameUniqueResults":
+        break;
+      case "emailImmediately":
+        draft.email.hasErrors = false;
+        draft.email.value = action.value;
+        break;
+      case "emailAfterDelay":
+        break;
+      case "emailUniqueResults":
+        break;
+      case "passwordImmediately":
+        draft.password.hasErrors = false;
+        draft.password.value = action.value;
+        break;
+      case "passwordAfterDelay":
+        break;
+      case "submitForm":
+        break;
+      default:
+        break;
     }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
   }
 
   return (
@@ -40,7 +96,12 @@ export default function HomeGuest() {
                 <small>Username</small>
               </label>
               <input
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={(e) => {
+                  dispatch({
+                    type: "usernameImmediately",
+                    value: e.target.value
+                  });
+                }}
                 id="username-register"
                 name="username"
                 className="form-control"
@@ -48,13 +109,28 @@ export default function HomeGuest() {
                 placeholder="Pick a username"
                 autoComplete="off"
               />
+              <CSSTransition
+                in={state.username.hasErrors}
+                timeout={330}
+                classNames="liveValidateMessage"
+                unmountOnExit
+              >
+                <div className="alert alert-danger small liveValidateMessage">
+                  {state.username.message}
+                </div>
+              </CSSTransition>
             </div>
             <div className="form-group">
               <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
               <input
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) =>
+                  dispatch({
+                    type: "emailImmediately",
+                    value: e.target.value
+                  })
+                }
                 id="email-register"
                 name="email"
                 className="form-control"
@@ -68,7 +144,12 @@ export default function HomeGuest() {
                 <small>Password</small>
               </label>
               <input
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) =>
+                  dispatch({
+                    type: "passwordImmediately",
+                    value: e.target.value
+                  })
+                }
                 id="password-register"
                 name="password"
                 className="form-control"
