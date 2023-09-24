25de16c725f43505d3f98359b9ec587ceaaa2415
iamverg <dev01.dash@gmail.com>
2023-09-24 18:23:47 +0200

    fix typo

--- app/components/Header.js
+++ app/components/Header.js
@@ -17,7 +17,7 @@ export default function Header(props) {
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
         <h4 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
-            Forum!
+            Forum
           </Link>
         </h4>
         {!props.staticEmpty ? headerContent : ""}
